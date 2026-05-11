9d0cbc23cd18ed4c14f0ff2cb56cc73b7480195b
blitz-003 <elmanpathan@gmail.com>
2026-05-11 20:04:36 +0600

    first commit. Have setup the skeleton for structure. Vite + react + tailwind + shadcn

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Add Button</Button>
+    </>
+  );
+}
+
+export default App;
